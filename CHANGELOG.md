de2bb048f0d9c9aa9565c7af9639b41a95225c27
Sattvik Chakravarthy <sattvik@supertokens.com>
2024-02-09 12:42:06 +0530

    fix: adds idle timeout and minimum idle configs (#95)
    * fix: idle connection configs
    
    * test: protected configs

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -11,6 +11,7 @@ to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 - Fixes the issue where passwords were inadvertently logged in the logs.
 - Adds tests to check connection pool behaviour.
+- Adds `mysql_idle_connection_timeout` and `mysql_minimum_idle_connections` configs to control active connections to the database.
 
 ## [5.0.5] - 2023-12-06
 
--- src/test/java/io/supertokens/storage/mysql/test/DbConnectionPoolTest.java
+++ src/test/java/io/supertokens/storage/mysql/test/DbConnectionPoolTest.java
@@ -64,6 +64,7 @@ public class DbConnectionPoolTest {
         TestingProcessManager.TestingProcess process = TestingProcessManager.start(args, false);
         FeatureFlagTestContent.getInstance(process.getProcess())
                 .setKeyValue(FeatureFlagTestContent.ENABLED_FEATURES, new EE_FEATURES[]{EE_FEATURES.MULTI_TENANCY});
+        Utils.setValueInConfig("mysql_minimum_idle_connections", "10");
         process.startProcess();
         assertNotNull(process.checkOrWaitForEvent(ProcessState.PROCESS_STATE.STARTED));
 
@@ -87,6 +88,7 @@ public class DbConnectionPoolTest {
 
         // change connection pool size
         config.addProperty("mysql_connection_pool_size", 20);
+        config.addProperty("mysql_minimum_idle_connections", 20);
 
         Multitenancy.addNewOrUpdateAppOrTenant(process.getProcess(), new TenantConfig(
                 new TenantIdentifier(null, null, "t1"),
@@ -118,6 +120,7 @@ public class DbConnectionPoolTest {
             TestingProcessManager.TestingProcess process = TestingProcessManager.start(args, false);
             FeatureFlagTestContent.getInstance(process.getProcess())
                     .setKeyValue(FeatureFlagTestContent.ENABLED_FEATURES, new EE_FEATURES[]{EE_FEATURES.MULTI_TENANCY});
+            Utils.setValueInConfig("mysql_minimum_idle_connections", "10");
             process.startProcess();
             assertNotNull(process.checkOrWaitForEvent(ProcessState.PROCESS_STATE.STARTED));
 
@@ -127,6 +130,7 @@ public class DbConnectionPoolTest {
             JsonObject config = new JsonObject();
             start.modifyConfigToAddANewUserPoolForTesting(config, 1);
             config.addProperty("mysql_connection_pool_size", 300);
+            config.addProperty("mysql_minimum_idle_connections", 300);
             AtomicLong firstErrorTime = new AtomicLong(-1);
             AtomicLong successAfterErrorTime = new AtomicLong(-1);
             AtomicInteger errorCount = new AtomicInteger(0);
@@ -190,6 +194,7 @@ public class DbConnectionPoolTest {
 
             // change connection pool size
             config.addProperty("mysql_connection_pool_size", 200);
+            config.addProperty("mysql_minimum_idle_connections", 200);
 
             Multitenancy.addNewOrUpdateAppOrTenant(process.getProcess(), new TenantConfig(
                     new TenantIdentifier(null, null, "t1"),
@@ -232,5 +237,166 @@ public class DbConnectionPoolTest {
         }
 
         fail(); // tried 5 times
+    }
+
+
+    @Test
+    public void testMinimumIdleConnections() throws Exception {
+        String[] args = {"../"};
+        {
+            TestingProcessManager.TestingProcess process = TestingProcessManager.start(args, false);
+            FeatureFlagTestContent.getInstance(process.getProcess())
+                    .setKeyValue(FeatureFlagTestContent.ENABLED_FEATURES, new EE_FEATURES[]{EE_FEATURES.MULTI_TENANCY});
+            Utils.setValueInConfig("mysql_connection_pool_size", "20");
+            Utils.setValueInConfig("mysql_minimum_idle_connections", "10");
+            Utils.setValueInConfig("mysql_idle_connection_timeout", "30000");
+            process.startProcess();
+            assertNotNull(process.checkOrWaitForEvent(ProcessState.PROCESS_STATE.STARTED));
+
+            Thread.sleep(65000); // let the idle connections time out
+
+            Start start = (Start) StorageLayer.getBaseStorage(process.getProcess());
+            assertEquals(10, start.getDbActivityCount("supertokens"));
+
+            process.kill();
+            assertNotNull(process.checkOrWaitForEvent(ProcessState.PROCESS_STATE.STOPPED));
+        }
+    }
+
+    @Test
+    public void testMinimumIdleConnectionForTenants() throws Exception {
+        String[] args = {"../"};
+
+        TestingProcessManager.TestingProcess process = TestingProcessManager.start(args, false);
+        FeatureFlagTestContent.getInstance(process.getProcess())
+                .setKeyValue(FeatureFlagTestContent.ENABLED_FEATURES, new EE_FEATURES[]{EE_FEATURES.MULTI_TENANCY});
+        Utils.setValueInConfig("mysql_minimum_idle_connections", "10");
+        process.startProcess();
+        assertNotNull(process.checkOrWaitForEvent(ProcessState.PROCESS_STATE.STARTED));
+
+        Start start = (Start) StorageLayer.getBaseStorage(process.getProcess());
+        assertEquals(10, start.getDbActivityCount("supertokens"));
+
+        JsonObject config = new JsonObject();
+        start.modifyConfigToAddANewUserPoolForTesting(config, 1);
+
+        Multitenancy.addNewOrUpdateAppOrTenant(process.getProcess(), new TenantConfig(
+                new TenantIdentifier(null, null, "t1"),
+                new EmailPasswordConfig(true),
+                new ThirdPartyConfig(true, null),
+                new PasswordlessConfig(true),
+                config
+        ), false);
+
+        Thread.sleep(1000); // let the new tenant be ready
+
+        assertEquals(10, start.getDbActivityCount("st1"));
+
+        // change connection pool size
+        config.addProperty("mysql_connection_pool_size", 20);
+        config.addProperty("mysql_minimum_idle_connections", 5);
+
+        Multitenancy.addNewOrUpdateAppOrTenant(process.getProcess(), new TenantConfig(
+                new TenantIdentifier(null, null, "t1"),
+                new EmailPasswordConfig(true),
+                new ThirdPartyConfig(true, null),
+                new PasswordlessConfig(true),
+                config
+        ), false);
+
+        Thread.sleep(2000); // let the new tenant be ready
+
+        assertEquals(5, start.getDbActivityCount("st1"));
+
+        // delete tenant
+        Multitenancy.deleteTenant(new TenantIdentifier(null, null, "t1"), process.getProcess());
+        Thread.sleep(2000); // let the tenant be deleted
+
+        assertEquals(0, start.getDbActivityCount("st1"));
+
+        process.kill();
+        assertNotNull(process.checkOrWaitForEvent(ProcessState.PROCESS_STATE.STOPPED));
+    }
+
+    @Test
+    public void testIdleConnectionTimeout() throws Exception {
+        String[] args = {"../"};
+
+        TestingProcessManager.TestingProcess process = TestingProcessManager.start(args, false);
+        FeatureFlagTestContent.getInstance(process.getProcess())
+                .setKeyValue(FeatureFlagTestContent.ENABLED_FEATURES, new EE_FEATURES[]{EE_FEATURES.MULTI_TENANCY});
+        Utils.setValueInConfig("mysql_minimum_idle_connections", "10");
+        process.startProcess();
+        assertNotNull(process.checkOrWaitForEvent(ProcessState.PROCESS_STATE.STARTED));
+
+        Start start = (Start) StorageLayer.getBaseStorage(process.getProcess());
+        assertEquals(10, start.getDbActivityCount("supertokens"));
+
+        JsonObject config = new JsonObject();
+        start.modifyConfigToAddANewUserPoolForTesting(config, 1);
+        config.addProperty("mysql_connection_pool_size", 300);
+        config.addProperty("mysql_minimum_idle_connections", 5);
+        config.addProperty("mysql_idle_connection_timeout", 30000);
+
+        AtomicLong errorCount = new AtomicLong(0);
+
+        Multitenancy.addNewOrUpdateAppOrTenant(process.getProcess(), new TenantConfig(
+                new TenantIdentifier(null, null, "t1"),
+                new EmailPasswordConfig(true),
+                new ThirdPartyConfig(true, null),
+                new PasswordlessConfig(true),
+                config
+        ), false);
+
+        Thread.sleep(3000); // let the new tenant be ready
+
+        assertTrue(10 >= start.getDbActivityCount("st1"));
+
+        ExecutorService es = Executors.newFixedThreadPool(150);
+
+        for (int i = 0; i < 10000; i++) {
+            int finalI = i;
+            es.execute(() -> {
+                try {
+                    TenantIdentifier t1 = new TenantIdentifier(null, null, "t1");
+                    TenantIdentifierWithStorage t1WithStorage = t1.withStorage(StorageLayer.getStorage(t1, process.getProcess()));
+                    ThirdParty.signInUp(t1WithStorage, process.getProcess(), "google", "googleid"+ finalI, "user" +
+                            finalI + "@example.com");
+
+                } catch (StorageQueryException e) {
+                    errorCount.incrementAndGet();
+                    throw new RuntimeException(e);
+                } catch (EmailChangeNotAllowedException e) {
+                    errorCount.incrementAndGet();
+                    throw new RuntimeException(e);
+                } catch (TenantOrAppNotFoundException e) {
+                    errorCount.incrementAndGet();
+                    throw new RuntimeException(e);
+                } catch (BadPermissionException e) {
+                    errorCount.incrementAndGet();
+                    throw new RuntimeException(e);
+                }
+            });
+        }
+
+        es.shutdown();
+        es.awaitTermination(2, TimeUnit.MINUTES);
+
+        assertTrue(5 < start.getDbActivityCount("st1"));
+
+        assertEquals(0, errorCount.get());
+
+        Thread.sleep(65000); // let the idle connections time out
+
+        assertEquals(5, start.getDbActivityCount("st1"));
+
+        // delete tenant
+        Multitenancy.deleteTenant(new TenantIdentifier(null, null, "t1"), process.getProcess());
+        Thread.sleep(3000); // let the tenant be deleted
+
+        assertEquals(0, start.getDbActivityCount("st1"));
+
+        process.kill();
+        assertNotNull(process.checkOrWaitForEvent(ProcessState.PROCESS_STATE.STOPPED));
     }
 }
